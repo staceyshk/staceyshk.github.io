c6da3caa8b0308072d1f836d30a547bc56003e08
Stacey Shkuratoff <sshkuratoff@popsugar.com>
2016-10-08 19:46:26 -0700

    Renamed to index.html for github pages and added timetable loading

--- StudentTimetable.js
+++ StudentTimetable.js
@@ -1,7 +1,26 @@
 $(function() {
-function addStudentsToList(students) {
-  alert(students);
-}
 
-$.get(' https://crystal-pepsi.herokuapp.com/students', addStudentsToList);
+  $('.student-dropdown').change(getStudentTimetable);
+
+  $.get(' https://crystal-pepsi.herokuapp.com/students', addStudentsToList);
+
+  function addStudentsToList(students) {
+    for (index in students) {
+      $('.student-dropdown').append($('<option>', {value: students[index], text: students[index]}));
+    }
+
+    getStudentTimetable();
+  }
+
+  function processTimetable(timetable) {
+    console.log(timetable);
+  }
+
+  function getStudentTimetable() {
+    var student = $( '.student-dropdown option:selected').val();
+    var studentUrl = 'https://crystal-pepsi.herokuapp.com/students/:student_name/meetings'
+      .replace(':student_name', student);
+
+    $.get(studentUrl, processTimetable);
+  }
 });

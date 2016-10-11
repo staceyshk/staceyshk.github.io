897bbf145ca2efe5dafe936137bd407e9e233d14
Stacey Shkuratoff <sshkuratoff@popsugar.com>
2016-10-10 23:13:56 -0700

    Added percentage and a label to name dropdown

--- StudentTimetable.js
+++ StudentTimetable.js
@@ -1,26 +1,43 @@
 $(function() {
-
+  $.get('https://crystal-pepsi.herokuapp.com/students', addStudents);
   $('.student-dropdown').change(getStudentTimetable);
 
-  $.get(' https://crystal-pepsi.herokuapp.com/students', addStudentsToList);
-
-  function addStudentsToList(students) {
-    for (index in students) {
+  function addStudents(students) {
+    for (var index in students) {
       $('.student-dropdown').append($('<option>', {value: students[index], text: students[index]}));
     }
 
     getStudentTimetable();
   }
 
-  function processTimetable(timetable) {
-    console.log(timetable);
-  }
-
   function getStudentTimetable() {
-    var student = $( '.student-dropdown option:selected').val();
+    var student = $( '.student-dropdown option:selected').text();
     var studentUrl = 'https://crystal-pepsi.herokuapp.com/students/:student_name/meetings'
       .replace(':student_name', student);
 
     $.get(studentUrl, processTimetable);
+  }
+
+  function processTimetable(meetings) {
+    var instructionalMeetings = [];
+
+    for (var index in meetings) {
+      var tags = meetings[index].tags;
+      if (tags.indexOf('instructional') > -1) {
+        instructionalMeetings.push(meetings[index]);
+      }
+    }
+
+    var percentage = getTotalDuration(instructionalMeetings)/getTotalDuration(meetings) * 100;
+    $('.duration-text').text(percentage);
+  }
+
+  function getTotalDuration(meetings) {
+    var duration = 0;
+    for (var index in meetings) {
+       duration += meetings[index].duration;
+    }
+
+    return duration;
   }
 });
